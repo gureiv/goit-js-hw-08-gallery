c0082510e88a481aa510b54ef213f29823b23c52
Gurei Vitaliy <gureivitalii@gmail.com>
2020-10-22 00:54:54 +0300

    finish

--- home.js
+++ home.js
@@ -1,25 +1,51 @@
 import products from './gallery-items.js';
 console.log(products);
-
-console.log(galleryPicture(products));
-function galleryPicture(products) {
-    return products.map(({ preview, original, description }) => {
-        return
-        `<li class="gallery_item">
-            <a
-                class="gallery_link"
-                href="${original}"
-            >
-                <img
-                    class="gallery_image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                />
-            </a>
-        </li>` ;
-    }).join('');
-};
-console.log(galleryPicture(products));
 const galleryContainer = document.querySelector('.js-gallery');
-galleryContainer.insertAdjacentHTML('beforeend', galleryPicture(products));
+const galleryImage = galleryPicture(products);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
+function galleryPicture(products) {
+    return products
+        .map(({ preview, original, description }) => {
+         let i = `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+            return i;
+        })
+        .join("");
+}
+
+galleryContainer.addEventListener('click', onClick);
+const lightBox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+function onClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    } 
+    console.log(evt.target.alt);
+    lightBox.classList.add('is-open');
+    lightboxImage.src = evt.target.dataset.source;
+    lightboxImage.alt = evt.target.alt;
+}
+
+
+const closeModal = document.querySelector('button[data-action="close-lightbox"]');
+closeModal.addEventListener('click', onCloseModal);
+function onCloseModal(evt) {
+    if (evt.target.nodeName !== 'BUTTON') {
+        return;
+    } 
+    lightBox.classList.remove('is-open');
+    lightboxImage.src = '';
+    lightboxImage.alt = '';
+}
